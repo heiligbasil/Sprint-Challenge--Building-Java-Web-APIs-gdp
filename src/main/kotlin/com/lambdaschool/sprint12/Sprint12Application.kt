600b3d2798ab88f63ff07790734d63ee2ef77ba2
heiligbasil <basilief@yahoo.com>
2019-06-07 08:48:56 -0700

    Made the main method static, added a static object list, and servlets

--- src/main/kotlin/com/lambdaschool/sprint12/Sprint12Application.kt
+++ src/main/kotlin/com/lambdaschool/sprint12/Sprint12Application.kt
@@ -2,11 +2,28 @@ package com.lambdaschool.sprint12
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
+@EnableWebMvc
 @SpringBootApplication
-class Sprint12Application
-
-fun main(args: Array<String>)
+open class Sprint12Application
 {
-    runApplication<Sprint12Application>(*args)
+    companion object
+    {
+        private lateinit var ourGdpList: GdpList
+
+        @JvmStatic
+        fun main(args: Array<String>)
+        {
+            ourGdpList = GdpList()
+            val ctx: ConfigurableApplicationContext = runApplication<Sprint12Application>(*args)
+
+            val dispatcherServlet: DispatcherServlet = ctx.getBean("dispatcherServlet") as DispatcherServlet
+            dispatcherServlet.setThrowExceptionIfNoHandlerFound(true)
+        }
+
+        fun getOurGdpList(): GdpList = ourGdpList
+    }
 }
