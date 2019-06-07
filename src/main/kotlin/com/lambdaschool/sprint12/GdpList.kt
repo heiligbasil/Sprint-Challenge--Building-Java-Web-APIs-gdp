c80bd3f62dda5272995af2c5335087189eea057b
heiligbasil <basilief@yahoo.com>
2019-06-07 08:40:02 -0700

    Added the Gdp List class

---
+++ src/main/kotlin/com/lambdaschool/sprint12/GdpList.kt
@@ -0,0 +1,140 @@
+package com.lambdaschool.sprint12
+
+import com.lambdaschool.sprint12.model.Gdp
+
+class GdpList
+{
+    val gdpList: MutableList<Gdp> = mutableListOf<Gdp>()
+
+    init
+    {
+        gdpList.add(Gdp("United States", "20513000".toLong()))
+        gdpList.add(Gdp("China", "13457267".toLong()))
+        gdpList.add(Gdp("Japan", "5070626".toLong()))
+        gdpList.add(Gdp("Germany", "4029140".toLong()))
+        gdpList.add(Gdp("United Kingdom", "2808899".toLong()))
+        gdpList.add(Gdp("France", "2794696".toLong()))
+        gdpList.add(Gdp("India", "2689992".toLong()))
+        gdpList.add(Gdp("Italy", "2086911".toLong()))
+        gdpList.add(Gdp("Brazil", "1909386".toLong()))
+        gdpList.add(Gdp("Canada", "1733706".toLong()))
+        gdpList.add(Gdp("South Korea", "1655608".toLong()))
+        gdpList.add(Gdp("Russia", "1576488".toLong()))
+        gdpList.add(Gdp("Spain", "1437047".toLong()))
+        gdpList.add(Gdp("Australia", "1427767".toLong()))
+        gdpList.add(Gdp("Mexico", "1199264".toLong()))
+        gdpList.add(Gdp("Indonesia", "1005268".toLong()))
+        gdpList.add(Gdp("Netherlands", "909887".toLong()))
+        gdpList.add(Gdp("Saudi Arabia", "769878".toLong()))
+        gdpList.add(Gdp("Turkey", "713513".toLong()))
+        gdpList.add(Gdp("Switzerland", "709118".toLong()))
+        gdpList.add(Gdp("Taiwan", "602678".toLong()))
+        gdpList.add(Gdp("Sweden", "554659".toLong()))
+        gdpList.add(Gdp("Poland", "549478".toLong()))
+        gdpList.add(Gdp("Belgium", "536055".toLong()))
+        gdpList.add(Gdp("Thailand", "490120".toLong()))
+        gdpList.add(Gdp("Argentina", "475429".toLong()))
+        gdpList.add(Gdp("Austria", "459401".toLong()))
+        gdpList.add(Gdp("Norway", "441439".toLong()))
+        gdpList.add(Gdp("United Arab Emirates", "432612".toLong()))
+        gdpList.add(Gdp("Iran", "430082".toLong()))
+        gdpList.add(Gdp("Nigeria", "397472".toLong()))
+        gdpList.add(Gdp("South Africa", "376679".toLong()))
+        gdpList.add(Gdp("Ireland", "366448".toLong()))
+        gdpList.add(Gdp("Israel", "365599".toLong()))
+        gdpList.add(Gdp("Hong Kong", "360315".toLong()))
+        gdpList.add(Gdp("Denmark", "354683".toLong()))
+        gdpList.add(Gdp("Malaysia", "347290".toLong()))
+        gdpList.add(Gdp("Singapore", "346621".toLong()))
+        gdpList.add(Gdp("Colombia", "336940".toLong()))
+        gdpList.add(Gdp("Philippines", "331678".toLong()))
+        gdpList.add(Gdp("Pakistan", "306987".toLong()))
+        gdpList.add(Gdp("Chile", "299887".toLong()))
+        gdpList.add(Gdp("Bangladesh", "286275".toLong()))
+        gdpList.add(Gdp("Finland", "276553".toLong()))
+        gdpList.add(Gdp("Egypt", "249471".toLong()))
+        gdpList.add(Gdp("Czech Republic", "244540".toLong()))
+        gdpList.add(Gdp("Vietnam", "241434".toLong()))
+        gdpList.add(Gdp("Romania", "239440".toLong()))
+        gdpList.add(Gdp("Portugal", "237962".toLong()))
+        gdpList.add(Gdp("Iraq", "230911".toLong()))
+        gdpList.add(Gdp("Peru", "228944".toLong()))
+        gdpList.add(Gdp("Greece", "218057".toLong()))
+        gdpList.add(Gdp("New Zealand", "205997".toLong()))
+        gdpList.add(Gdp("Algeria", "188342".toLong()))
+        gdpList.add(Gdp("Qatar", "188295".toLong()))
+        gdpList.add(Gdp("Kazakhstan", "184209".toLong()))
+        gdpList.add(Gdp("Hungary", "156393".toLong()))
+        gdpList.add(Gdp("Kuwait", "144523".toLong()))
+        gdpList.add(Gdp("Ukraine", "126390".toLong()))
+        gdpList.add(Gdp("Morocco", "118178".toLong()))
+        gdpList.add(Gdp("Angola", "114504".toLong()))
+        gdpList.add(Gdp("Ecuador", "107266".toLong()))
+        gdpList.add(Gdp("Slovakia", "106940".toLong()))
+        gdpList.add(Gdp("Puerto Rico", "104557".toLong()))
+        gdpList.add(Gdp("Venezuela", "96328".toLong()))
+        gdpList.add(Gdp("Sri Lanka", "92504".toLong()))
+        gdpList.add(Gdp("Kenya", "89591".toLong()))
+        gdpList.add(Gdp("Ethiopia", "83836".toLong()))
+        gdpList.add(Gdp("Oman", "81682".toLong()))
+        gdpList.add(Gdp("Dominican Republic", "81103".toLong()))
+        gdpList.add(Gdp("Guatemala", "79109".toLong()))
+        gdpList.add(Gdp("Syria", "77460".toLong()))
+        gdpList.add(Gdp("Myanmar", "71543".toLong()))
+        gdpList.add(Gdp("Luxembourg", "68993".toLong()))
+        gdpList.add(Gdp("Panama", "66031".toLong()))
+        gdpList.add(Gdp("Bulgaria", "63651".toLong()))
+        gdpList.add(Gdp("Uruguay", "60933".toLong()))
+        gdpList.add(Gdp("Costa Rica", "60816".toLong()))
+        gdpList.add(Gdp("Croatia", "59971".toLong()))
+        gdpList.add(Gdp("Belarus", "56934".toLong()))
+        gdpList.add(Gdp("Lebanon", "56709".toLong()))
+        gdpList.add(Gdp("Tanzania", "55645".toLong()))
+        gdpList.add(Gdp("Slovenia", "54969".toLong()))
+        gdpList.add(Gdp("Macau", "53961".toLong()))
+        gdpList.add(Gdp("Lithuania", "52468".toLong()))
+        gdpList.add(Gdp("Ghana", "51815".toLong()))
+        gdpList.add(Gdp("Serbia", "47564".toLong()))
+        gdpList.add(Gdp("Cote d'Ivoire", "45875".toLong()))
+        gdpList.add(Gdp("Azerbaijan", "45592".toLong()))
+        gdpList.add(Gdp("Uzbekistan", "43303".toLong()))
+        gdpList.add(Gdp("Libya", "43236".toLong()))
+        gdpList.add(Gdp("Democratic Republic of the Congo", "42692".toLong()))
+        gdpList.add(Gdp("Turkmenistan", "42764".toLong()))
+        gdpList.add(Gdp("Jordan", "41869".toLong()))
+        gdpList.add(Gdp("Paraguay", "41851".toLong()))
+        gdpList.add(Gdp("Bolivia", "41833".toLong()))
+        gdpList.add(Gdp("Tunisia", "41662".toLong()))
+        gdpList.add(Gdp("Bahrain", "39300".toLong()))
+        gdpList.add(Gdp("Cameroon", "38445".toLong()))
+        gdpList.add(Gdp("Latvia", "34286".toLong()))
+        gdpList.add(Gdp("Sudan", "33249".toLong()))
+    }
+
+    fun findGdp(tester: CheckGdp): Gdp?
+    {
+        for (gdp: Gdp in gdpList)
+        {
+            if (tester.test(gdp))
+            {
+                return gdp
+            }
+        }
+        return null
+    }
+
+    fun findGdps(tester: CheckGdp): List<Gdp>
+    {
+        val tempGdpList: MutableList<Gdp> = mutableListOf<Gdp>()
+
+        for (gdp: Gdp in gdpList)
+        {
+            if (tester.test(gdp))
+            {
+                tempGdpList.add(gdp)
+            }
+        }
+
+        return tempGdpList
+    }
+}
