fac26fc9dbdb5306afc68a115bcda3663acb4779
heiligbasil <basilief@yahoo.com>
2019-06-07 08:52:17 -0700

    Added the model for the error messages

---
+++ src/main/kotlin/com/lambdaschool/sprint12/model/ErrorDetail.kt
@@ -0,0 +1,20 @@
+package com.lambdaschool.sprint12.model
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+data class ErrorDetail(
+        var title: String? = null,
+        var status: Int = 0,
+        var detail: String? = null,
+        var timestamp: String? = null,
+        var developerMessage: String? = null,
+        var errors: MutableMap<String, MutableList<ValidationError>> = HashMap<String, MutableList<ValidationError>>()
+)
+{
+    fun setTimestamp(timestamp: Long?)
+    {
+        this.timestamp = SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z").format(Date(timestamp!!))
+    }
+}
+
