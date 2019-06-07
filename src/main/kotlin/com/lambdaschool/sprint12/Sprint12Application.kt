8a7690397373a7d5d631edc1c3e39c556cdbb359
heiligbasil <basilief@yahoo.com>
2019-06-07 08:13:41 -0700

    Basil Havens - Sprint-Challenge--Building-Java-Web-APIs-gdp

---
+++ src/main/kotlin/com/lambdaschool/sprint12/Sprint12Application.kt
@@ -0,0 +1,12 @@
+package com.lambdaschool.sprint12
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sprint12Application
+
+fun main(args: Array<String>)
+{
+    runApplication<Sprint12Application>(*args)
+}
---
+++ src/test/kotlin/com/lambdaschool/sprint12/Sprint12ApplicationTests.kt
@@ -0,0 +1,18 @@
+package com.lambdaschool.sprint12
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Sprint12ApplicationTests
+{
+
+    @Test
+    fun contextLoads()
+    {
+    }
+
+}
